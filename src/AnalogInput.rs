5d6d9a9c382f89cd2cd7ae805f34ad5b9ab89f4b
Connor Worley <connorbworley@gmail.com>
2017-10-05 19:44:27 -0700

    Initial commit

---
+++ src/AnalogInput.rs
@@ -0,0 +1,47 @@
+extern crate frchal;
+use frchal::*;
+
+struct AnalogInput {
+    m_channel: i32,
+    m_port: HAL_AnalogInputHandle,
+//    m_accumulatorOffset: i64,
+}
+
+impl AnalogInput {
+    const kAccumulatorModuleNumber: i32 = 1;
+    const kAccumulatorNumChannels: i32 = 2;
+    const kAccumulatorChannels: [i32; 2] = [0, 1];
+
+    fn fromChannel(channel: i32) -> Self {
+        // todo: bounds checking on channel
+        let port = unsafe { HAL_GetPort(channel) };
+        let mut status: i32 = 0;
+        let analogPort = unsafe { HAL_InitializeAnalogInputPort(port, &mut status) };
+        if status != 0 {
+            // todo: error
+        }
+        return AnalogInput {
+            m_channel: channel,
+            m_port: analogPort
+        };
+    }
+    fn getValue(&self) -> i32 {
+        let mut status: i32 = 0;
+        let value = unsafe { HAL_GetAnalogValue(self.m_port, &mut status) };
+        // todo: check error status
+        return value;
+    }
+    fn getVoltage(&self) -> f64 {
+        let mut status: i32 = 0;
+        let value = unsafe { HAL_GetAnalogVoltage(self.m_port, &mut status) };
+        // todo: check error status
+        return value;
+    }
+}
+
+impl Drop for AnalogInput {
+    fn drop(&mut self) {
+        unsafe { HAL_FreeAnalogInputPort(self.m_port); }
+        self.m_port = HAL_kInvalidHandle as i32;
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+include!("AnalogInput.rs");
