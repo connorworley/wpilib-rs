4455e6ae24074aa1c2923be231ad6c00a945ba62
Connor Worley <connorbworley@gmail.com>
2017-10-05 19:59:40 -0700

    Make AnalogInput public

--- src/AnalogInput.rs
+++ src/AnalogInput.rs
@@ -1,7 +1,7 @@
 extern crate frchal;
 use frchal::*;
 
-struct AnalogInput {
+pub struct AnalogInput {
     m_channel: i32,
     m_port: HAL_AnalogInputHandle,
 //    m_accumulatorOffset: i64,
@@ -12,7 +12,7 @@ impl AnalogInput {
     const kAccumulatorNumChannels: i32 = 2;
     const kAccumulatorChannels: [i32; 2] = [0, 1];
 
-    fn fromChannel(channel: i32) -> Self {
+    pub fn fromChannel(channel: i32) -> Self {
         // todo: bounds checking on channel
         let port = unsafe { HAL_GetPort(channel) };
         let mut status: i32 = 0;
@@ -25,13 +25,13 @@ impl AnalogInput {
             m_port: analogPort
         };
     }
-    fn getValue(&self) -> i32 {
+    pub fn getValue(&self) -> i32 {
         let mut status: i32 = 0;
         let value = unsafe { HAL_GetAnalogValue(self.m_port, &mut status) };
         // todo: check error status
         return value;
     }
-    fn getVoltage(&self) -> f64 {
+    pub fn getVoltage(&self) -> f64 {
         let mut status: i32 = 0;
         let value = unsafe { HAL_GetAnalogVoltage(self.m_port, &mut status) };
         // todo: check error status
